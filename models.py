f6f90803dcdb29cbf3993d98ffa26cda167f859a
RonaldoKeng <ronaldokeng0815@gmail.com>
2019-09-13 10:50:48 +1000

    first commit

---
+++ models.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    isbn = db.Column(db.Integer, nullable=False)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
